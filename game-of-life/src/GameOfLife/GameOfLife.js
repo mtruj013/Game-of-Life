7fd1b04dcdc66dc7615aee8d3e7eb1aabc145068
Maria Trujillo <mptruj015@gmail.com>
2020-09-25 00:00:18 +0200

    added images, styling, and grid size option functionality

--- game-of-life/src/GameOfLife/GameOfLife.js
+++ game-of-life/src/GameOfLife/GameOfLife.js
@@ -2,6 +2,10 @@
 import React from 'react';
 // import style sheet
 import './GameOfLife.scss';
+//import presets
+import blinker_2 from './gifs/blinker_2.gif'
+import blinker_3 from './gifs/blinker_3.gif'
+import blinker_4 from './gifs/blinker_4.gif'
 
 // create class GameofLife using react component
 export default class GameofLife extends React.Component {
@@ -24,13 +28,23 @@ export default class GameofLife extends React.Component {
         super();
 
         this.state = {
-            cells: this.initializedCells(), 
+            cells: [],
             isGameRunning: false,
-            generation: 0
+            generation: 0,
+            grid: {
+                columns: 25,
+                rows: 25
+            }
         }
-
+        
         // calls the live function each second
         setInterval(() => this.life(), 500)
+    }
+
+    componentDidMount(){
+        this.setState({
+            cells: this.initializedCells()
+        })
     }
 
 
@@ -40,10 +54,10 @@ export default class GameofLife extends React.Component {
         let cells = [];
 
         // fills the grid
-        for (let colIndex = 0; colIndex < GameofLife.grid.columns; colIndex++) {
+        for (let colIndex = 0; colIndex < this.state.grid.columns; colIndex++) {
             // fills column with an empty array
             cells[colIndex] = [];
-            for (let rowIndex = 0; rowIndex < GameofLife.grid.rows; rowIndex++) {
+            for (let rowIndex = 0; rowIndex < this.state.grid.rows; rowIndex++) {
                 // populates the cell 
                 cells[colIndex][rowIndex] = GameofLife.cellState.OFF;
             }
@@ -60,10 +74,10 @@ export default class GameofLife extends React.Component {
         const newCells = [];
         
         // fills grid
-        for(let colIndex = 0; colIndex < GameofLife.grid.columns; colIndex++){
+        for(let colIndex = 0; colIndex < this.state.grid.columns; colIndex++){
             // fills column with an empty array
             newCells[colIndex] = [];
-            for(let rowIndex = 0; rowIndex < GameofLife.grid.rows; rowIndex++){
+            for(let rowIndex = 0; rowIndex < this.state.grid.rows; rowIndex++){
                 // populates new cells
                 newCells[colIndex][rowIndex] = this.computeNewCellState(colIndex, rowIndex)
             }
@@ -88,6 +102,39 @@ export default class GameofLife extends React.Component {
             generation: 0
         })
     }
+
+    // generates a new grid depending on user choice
+    setNewGridSize(size) {
+        console.log(size)
+        switch (size) {
+            case "1":
+                this.setState({
+                    grid: {
+                        columns: 20,
+                        rows: 10
+                    }
+                })
+            break;
+            case "2":
+                this.setState({
+                    grid: {
+                        columns: 30,
+                        rows: 40
+                    }
+                })
+            break;
+            default:
+                this.setState({
+                    grid: {
+                        columns: 25,
+                        rows: 25
+                    }
+                })
+        }
+        console.log(this.state.grid)
+        this.clearGrid();
+    }
+
 
     // logic for updating the cell state while game is running 
     computeNewCellState(colIndex, rowIndex) {
@@ -147,10 +194,10 @@ export default class GameofLife extends React.Component {
             let newRowOffset = rowIndex + offSetY;
         
             // checks grid boundries
-            if (newColOffset < 0 || newColOffset > GameofLife.grid.columns - 1) {
+            if (newColOffset < 0 || newColOffset > this.state.grid.columns - 1) {
                 continue;
             }
-            if (newRowOffset < 0 || newRowOffset > GameofLife.grid.rows - 1){
+            if (newRowOffset < 0 || newRowOffset > this.state.grid.rows - 1){
                 continue;
             }
                     
@@ -234,15 +281,84 @@ export default class GameofLife extends React.Component {
         )
     }
 
+    // handle event listener
+    handleSelect(event) {
+      this.setNewGridSize(event.target.value);
+    }
+
+    // renders grid size dropdown 
+    renderGridSizeButton() {
+        return (
+            <select
+                id = "grid-size"
+                name= "size"
+                onChange={this.handleSelect.bind(this)}>
+                    <option value="1">20 x 10</option>
+                    <option value="2">30 x 40</option>
+                    <option value="3">25 x 25</option>
+                </select>
+        )
+    }
+
+    // renders button for user color choice
+    renderCellColorPicker() {
+        return(
+            <select
+            id= "color-choice"
+            name= "grid-color"
+            >
+                <option value="red">red</option>
+                <option value="blue"> blue</option>
+                <option value="green">green</option>
+            </select>
+        )
+    }
+
 
     // renders this component
     render() {
         return (
-            <div className="GameOfLife">
-                {this.rendercells()}
-                {this.renderStartButton()}
-                {this.renderClearButton()}
-        <h2>Generations: {this.state.generation}</h2>
+            <div className="container">
+                <h1>Game of Life</h1>
+                <div className="content">
+                    <div className="GameOfLife">
+                        {this.renderStartButton()}
+                        {this.renderClearButton()}
+                        {this.renderGridSizeButton()}
+                        {this.renderCellColorPicker()}
+                        {this.rendercells()}
+                        <h2>Generations: {this.state.generation}</h2>
+                    </div>
+                    <div className="presets">
+                        <h2>POPULAR PRESETS</h2>
+                            <img className="blinker_1" src={blinker_2} alt="preset 2"/>
+                            <h3>Preset 1</h3>
+                            <img className="blinker_2" src={blinker_3} alt="preset 3"/>
+                            <h3>Preset 2</h3>
+                            <img className="blinker_3" src={blinker_4} alt="preset 4"/>
+                            <h3>Preset 3</h3>
+                    </div>
+                    <div className="about">
+                        <div className="rules">
+                            <h2>RULES</h2>
+                            <p>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+
+                            <p>2. Any live cell with two or three live neighbours lives on to the next generation.</p>
+
+                            <p>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+
+                            <p>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
+                        </div>
+                        <div className="creator">
+                            <h2>ABOUT THE CREATOR,</h2>
+                            <h2>JOHN CONWAY</h2>
+                            <p>The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.[1] It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
+                        </div>
+                        
+                    </div>
+
+                    
+                </div>
             </div>
         )
     }
